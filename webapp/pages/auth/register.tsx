ed317ee18fec0785bf9998f52598432a8e0bb2e7
arun thulasidharan <arun@aruns-MBP-2.Dlink>
2021-12-06 21:10:04 +0530

    commenting signup button

--- webapp/pages/auth/register.tsx
+++ webapp/pages/auth/register.tsx
@@ -88,7 +88,7 @@ function Register(context: GetServerSidePropsContext, props: serverSideProps) {
           <ButtonSubmit className="form-control" />
         </Form>
       </Formik>
-      <div className="mt-3 text-center">
+      {/* <div className="mt-3 text-center">
         <Button
           href={authUtils.getExternalLoginUrl(
             props.appBaseUrl,
@@ -99,7 +99,7 @@ function Register(context: GetServerSidePropsContext, props: serverSideProps) {
         >
           Sign up with Google
         </Button>
-      </div>
+      </div> */}
     </GuestLayout>
   );
 }
