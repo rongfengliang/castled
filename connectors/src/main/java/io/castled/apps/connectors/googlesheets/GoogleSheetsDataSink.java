59e5e0c41f57d055a24b533cff14d6c07a7da4ad
arun thulasidharan <arun@aruns-MBP-2.Dlink>
2021-12-12 00:27:55 +0530

    google sheets

--- connectors/src/main/java/io/castled/apps/connectors/googlesheets/GoogleSheetsDataSink.java
+++ connectors/src/main/java/io/castled/apps/connectors/googlesheets/GoogleSheetsDataSink.java
@@ -1,5 +1,7 @@
 package io.castled.apps.connectors.googlesheets;
 
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.Spreadsheet;
 import io.castled.apps.DataSink;
 import io.castled.apps.models.DataSinkRequest;
 import io.castled.commons.models.AppSyncStats;
@@ -7,7 +9,9 @@ import io.castled.commons.models.AppSyncStats;
 public class GoogleSheetsDataSink implements DataSink {
     @Override
     public void syncRecords(DataSinkRequest dataSinkRequest) throws Exception {
-
+        GoogleSheetsAppConfig googleSheetsAppConfig = (GoogleSheetsAppConfig) dataSinkRequest.getExternalApp().getConfig();
+        Sheets sheetsService = GoogleSheetUtils.getSheets(googleSheetsAppConfig.getServiceAccountDetails());
+        Spreadsheet spreadsheet = sheetsService.spreadsheets().get(googleSheetsAppConfig.getSpreadSheetId()).execute();
     }
 
     @Override
