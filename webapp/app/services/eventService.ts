09bf34f2d0d8d61e90a0711e2811474040aa7382
arun thulasidharan <arun@aruns-MBP-2.Dlink>
2022-01-05 00:05:55 +0530

    removing tag manager

--- webapp/app/services/eventService.ts
+++ webapp/app/services/eventService.ts
@@ -1,6 +1,5 @@
 import { StringAnyMap } from "@/app/common/utils/types";
 import http from "@/app/services/http";
-import tagManager from "react-gtm-module";
 import { LoggedInUserDto } from "@/app/common/dtos/LoggedInUserDto";
 
 export default {
@@ -19,18 +18,10 @@ export default {
         userCreatedTs: user.createdTs,
       };
     }
-    if (process.browser) {
-      console.log("loading tag manager");
-      tagManager.initialize({
-        gtmId: "GTM-5PMX6V3",
-        dataLayer: userProps,
-      });
-      console.log("GTM Loaded", userProps);
-    }
+
   },
   send: (props: StringAnyMap) => {
     if (process.browser) {
-      tagManager.dataLayer({ dataLayer: { ...props } });
       http.post("/v1/tracking", props)
       .catch(() => {
         console.log(`tracking failed for event ${props.event}`)
