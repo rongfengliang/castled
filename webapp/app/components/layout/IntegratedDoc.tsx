4961b8da4d74c3780d5b72602f9fbce136af9f77
Naveen <iamnaveenpaul@gmail.com>
2022-02-01 00:17:12 +0530

    doc not found fixes

--- webapp/app/components/layout/IntegratedDoc.tsx
+++ webapp/app/components/layout/IntegratedDoc.tsx
@@ -11,26 +11,47 @@ export interface IntegratedDocProps {
 
 const IntegratedDoc = ({ category, connectorType }: IntegratedDocProps) => {
   const [markdown, setMarkdown] = useState<string>("");
+  const [markdownNotFound, setMarkdownNotFound] = useState<boolean>(false);
 
   let docImgUrl =
     process.env.DOC_APP_IMAGE_BASE_URL ||
     "https://raw.githubusercontent.com/castledio/castled/main/docs/static/"; // NP: This can be deleted and url can be loaded only from env.
 
   useEffect(() => {
-    documentationService.load(category, connectorType).then((markdown) => {
-      const metaPos = markdown.lastIndexOf("\n---\n");
-      setMarkdown(metaPos === -1 ? markdown : markdown.substring(metaPos + 5));
-    });
+    documentationService
+      .load(category, connectorType)
+      .then((markdown) => {
+        console.log(markdown);
+        const metaPos = markdown.lastIndexOf("\n---\n");
+        setMarkdownNotFound(markdown == "404: Not Found");
+        setMarkdown(
+          metaPos === -1 ? markdown : markdown.substring(metaPos + 5)
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, [category, connectorType]);
   return (
-    <ReactMarkdown
-      className="integrated-doc"
-      transformImageUri={(uri) =>
-        uri.startsWith("http") ? uri : `${docImgUrl}${uri}`
-      }
-    >
-      {markdown}
-    </ReactMarkdown>
+    <div>
+      {markdownNotFound ? (
+        <div className="doc-not-found">
+          <p>
+            We are working on the documentation of this {category}. If you face
+            any issues during the setup, please contact support on chat.
+          </p>
+        </div>
+      ) : (
+        <ReactMarkdown
+          className="integrated-doc"
+          transformImageUri={(uri) =>
+            uri.startsWith("http") ? uri : `${docImgUrl}${uri}`
+          }
+        >
+          {markdown}
+        </ReactMarkdown>
+      )}
+    </div>
   );
 };
 
