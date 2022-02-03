1cc320e4b94f0b7bf056780958931fc67003f158
arun thulasidharan <arun@aruns-MBP-2.Dlink>
2022-02-03 09:51:30 +0530

    rest api app sync config fix

--- connectors/src/main/java/io/castled/apps/connectors/restapi/RestApiAppSyncConfig.java
+++ connectors/src/main/java/io/castled/apps/connectors/restapi/RestApiAppSyncConfig.java
@@ -7,14 +7,23 @@ import io.castled.forms.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import static io.castled.forms.FormGroups.TUNNEL_GROUP;
+
 @Getter
 @Setter
+@GroupActivator(dependencies = {"bulk"}, group = "bulk")
 public class RestApiAppSyncConfig extends AppSyncConfig {
 
-    @FormField(required = false,type = FormFieldType.TEXT_BOX, title = "Batch Size", description = "Batch Size")
-    private Integer batchSize;
 
-    @FormField(required = false,type = FormFieldType.TEXT_BOX, title = "Parallelism", description = "Parallelism")
+    @FormField(required = false, type = FormFieldType.TEXT_BOX, title = "Parallelism", description = "Parallelism")
     private Integer parallelism;
 
+    @FormField(required = false, description = "Bulk update", title = "Enable bulk", schema = FormFieldSchema.BOOLEAN, type = FormFieldType.CHECK_BOX)
+    private boolean bulk;
+
+    @FormField(required = false, type = FormFieldType.TEXT_BOX, title = "Json Array path", placeholder = "parent.child.subchild", description = "Json Array Path", group = "bulk")
+    private Integer jsonPath;
+
+    @FormField(required = false, type = FormFieldType.TEXT_BOX, title = "Batch Size", description = "Batch Size", group = "bulk")
+    private Integer batchSize;
 }
